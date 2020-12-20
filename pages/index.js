a66629ff4ef62e1853d4c9a88fe3408d07ed248b
Jorge <jorgecbll@gmail.com>
2020-12-20 14:11:02 -0300

    removed head

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home({time}) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>server: {time}</h1>
      <h2>client: {`${new Date().getMinutes()}:${new Date().getSeconds()}`}</h2>
      <h3>revalidate: 10</h3>
